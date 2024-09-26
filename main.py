9659d1732405d9a9e81979041b6bb7041ec459e0
gui23dib <gui23dib@gmail.com>
2024-09-25 23:11:55 -0300

    add roullete mating pool selection method

--- main.py
+++ main.py
@@ -17,7 +17,18 @@ def mating_pool_tournament(population: list[ChromosomeClass]):
     result = random.sample(population, 2)
 
     pointer: int = random.randint(1, len(result[0].value) - 1)
-    result[0], result[1] = result[0].reproduce(result[1], pointer), result[1].reproduce(result[0], pointer)
+    result[0] = result[0].reproduce(result[1], pointer)
+    result[1] = result[1].reproduce(result[0], pointer)
+
+    return result
+
+def mating_pool_roulette(population: list[ChromosomeClass]):
+    total_net_fitness = sum([chromosome.fitness for chromosome in population])
+    result = random.choices(population, weights=[chromosome.fitness / total_net_fitness for chromosome in population], k=2)
+
+    pointer: int = random.randint(1, len(result[0].value) - 1)
+    result[0] = result[0].reproduce(result[1], pointer)
+    result[1] = result[1].reproduce(result[0], pointer)
 
     return result
 
@@ -26,28 +37,33 @@ def new_generation(population: list[ChromosomeClass], n_survivors: int = 2) -> l
 
 
 if __name__ == '__main__':
-    word_obj: str = str(input()).upper()
-    objective: list[int] = [ord(e) for e in word_obj]
+    try:
+        word_obj: str = str(input()).upper()
+        objective: list[int] = [ord(e) for e in word_obj]
 
-    max_iterations: int = 100000
-    gen_count: int = 0
+        max_iterations: int = 100000
+        gen_count: int = 0
 
-    population: PopulationClass = PopulationClass()
-    population.populate(len(objective), len(word_obj) * 10)
+        population: PopulationClass = PopulationClass()
+        population.populate(len(objective), len(word_obj) * 10)
 
-    while population.best_fitness != 1.0 and gen_count <= max_iterations:
-        population.population = fitness(population.population, objective)
-        population.sort_population() # sort population by fitness (already defines the best fitness)
+        while population.best_fitness != 1.0 and gen_count <= max_iterations:
+            population.population = fitness(population.population, objective)
+            population.sort_population() # sort population by fitness (already defines the best fitness)
 
-        print_stats(population, gen_count)
+            print_stats(population, gen_count)
 
-        new_population: list[ChromosomeClass] = new_generation(population.population, 2)
-        while len(new_population) < len(population.population):
-            for child in mating_pool_tournament(population.population):
-                new_population.append(ChromosomeClass(child.mutate()))
+            new_population: list[ChromosomeClass] = new_generation(population.population, 2)
+            while len(new_population) < len(population.population):
+                for child in mating_pool_roulette(population.population): # pick your mating pool method here
+                    new_population.append(ChromosomeClass(child.mutate()))
 
-        population.population = new_population
-        gen_count += 1
+            population.population = new_population
+            gen_count += 1
+    except:
+        print("No input given.")
+    except KeyboardInterrupt:
+        print("Process interrupted by user.")
 
     print("All generations have been processed.")
     print(f"Total generations: {gen_count}")
